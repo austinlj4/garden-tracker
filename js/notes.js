cfca58d9647ffced75350c8b8cb96eea4146e836
austinlj4 <austinalynn15@gmail.com>
2026-08-18 16:14:01 -0500

    Add functions to save and load plant notes

--- js/notes.js
+++ js/notes.js
@@ -1,1 +1,32 @@
+function savePlantNotes(plantId) {
+
+    const notes =
+        document.getElementById('personalPlantNotes').value;
+
+    const savedNotes =
+        JSON.parse(localStorage.getItem('plantNotes')) || {};
+
+    savedNotes[plantId] = notes;
+
+    localStorage.setItem(
+        'plantNotes',
+        JSON.stringify(savedNotes)
+    );
+
+    document.getElementById('plantNotesMessage').textContent =
+        'Notes saved!';
+
+}
+
+function loadPlantNotes(plantId) {
+
+    const savedNotes =
+        JSON.parse(localStorage.getItem('plantNotes')) || {};
+
+    const notes =
+        savedNotes[plantId] || '';
+
+    document.getElementById('personalPlantNotes').value =
+        notes;
+}
 
